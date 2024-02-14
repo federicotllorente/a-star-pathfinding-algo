ce763ac16f3020ea81d7496ea3c434a61dd33dbd
Federico Tejedor Llorente <federicotllorente@gmail.com>
2024-02-14 17:41:04 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const GRID_COLS = 5
+const GRID_ROWS = 5
+const grid = new Array()
+
+function setup() {
+  const app = document.getElementById('app')
+  const canvas = document.createElement('canvas')
+  canvas.width = 500
+  canvas.height = 500
+  app.appendChild(canvas)
+}
+
+setup()
